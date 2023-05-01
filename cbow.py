4009ab35bb383320a86299024bc937f7b81cc06d
aarcc530 <aidan.casey@aarcc530.com>
2023-05-01 00:24:32 -0400

    Lots of Commenting

--- cbow.py
+++ cbow.py
@@ -8,6 +8,7 @@ class CBOW(nn.Module):
         self.word_len = word_len
         self.emoji_len = emoji_len
 
+        # Create/Load Word Emebeddings, zeroing out the 0/period
         if word_embeddings is None:
             assert(emb_dim > 0)
             self.emb_dim = emb_dim
@@ -25,6 +26,7 @@ class CBOW(nn.Module):
             temp_state['weight'][0] = torch.zeros(self.emb_dim)
             self.word_embeddings.load_state_dict(temp_state)
 
+        # Create Emoji Embeddings, zeroing out 0/the period
         self.emoji_embeddings = nn.Embedding(emoji_len, self.emb_dim, sparse=True)
         temp_state2 = self.emoji_embeddings.state_dict()
         temp_state2['weight'][0] = torch.zeros(self.emb_dim)
@@ -32,11 +34,10 @@ class CBOW(nn.Module):
 
         assert(window > 0)
         self.window = window
-        self.learning_layer1 = nn.Sequential(
-            nn.Linear(self.emb_dim, hidden_size),
-            nn.Tanh()
-        )
-        self.learning_layer2 = nn.Sequential(
+
+
+        # As far as I can tell, it should just be embed -> linear -> softmax
+        self.learning_layer = nn.Sequential(
             #nn.Linear(hidden_size, word_len + emoji_len - 1),
             #nn.Linear(hidden_size * 4, word_len + emoji_len - 1),
             nn.Linear(self.emb_dim, word_len + emoji_len - 1),
@@ -45,7 +46,7 @@ class CBOW(nn.Module):
         )
 
     def forward(self, X: torch.Tensor):
-        # Assuming this is data loaded
+        # This is a mess I came up with on how to word embed all of these across the two since negatives are emoji
         zeroes = torch.zeros_like(X)
         zeroed_out_emojis = torch.where(X < 0, zeroes, X)
         zeroed_out_words = torch.where(X < 0, torch.negative(X), zeroes)
@@ -56,16 +57,21 @@ class CBOW(nn.Module):
         #out1 = self.learning_layer1(embedded)
         #out2 = torch.mean(out1, dim=1)
         #out2 = torch.flatten(out1, start_dim=1)
-        out2 = torch.mean(embedded, dim=1)
-        out3 = self.learning_layer2(out2)
 
-        return out3
+        # Take Mean of input, then stick it through the learning layer
+        out1 = torch.mean(embedded, dim=1)
+        out2 = self.learning_layer(out1)
+
+        return out2
     
-    def get_word_embedding(self, word_num):
-        if word_num < 0:
-            return self.emoji_embeddings(-word_num)
-        return self.word_embeddings(word_num)
+    # Allows retrieval of embeddings
+    def get_word_embeddings(self, words):
+        emojis = self.emoji_embeddings(words)
+        words = self.word_embeddings(words)
+        return torch.where(words < 0, emojis, words)
     
+
+    # Converts probabilities into predictions
     def predict(self, X: torch.tensor):
         if X.ndim == 1:
             results = torch.argmax(X)
@@ -75,63 +81,74 @@ class CBOW(nn.Module):
         return torch.where(results >= self.word_len, results2, results)
     
 
-    class SkipGram(nn.Module):
-        def __init__(self, word_len, emoji_len, word_embeddings=None, freeze_pretrained_words=True, emb_dim=50, window=4, hidden_size=10):
-            super(CBOW, self).__init__()
+class SkipGram(nn.Module):
+    def __init__(self, word_len, emoji_len, word_embeddings=None, freeze_pretrained_words=True, emb_dim=50, window=4, hidden_size=10):
+        super(CBOW, self).__init__()
 
-            self.word_len = word_len
-            self.emoji_len = emoji_len
+        self.word_len = word_len
+        self.emoji_len = emoji_len
 
-            if word_embeddings is None:
-                assert(emb_dim > 0)
-                self.emb_dim = emb_dim
 
-                self.word_embeddings = nn.Embedding(word_len, self.emb_dim)
-                temp_state = self.word_embeddings.state_dict()
-                temp_state['weight'][0] = torch.zeros(self.emb_dim)
-                self.word_embeddings.load_state_dict(temp_state)
-            else:
-                self.emb_dim = len(word_embeddings[0])
-                self.word_len - len(word_embeddings)
+        # Create/Load Word Emebeddings, zeroing out the 0/period
+        if word_embeddings is None:
+            assert(emb_dim > 0)
+            self.emb_dim = emb_dim
+            self.word_embeddings = nn.Embedding(word_len, self.emb_dim)
+            temp_state = self.word_embeddings.state_dict()
+            temp_state['weight'][0] = torch.zeros(self.emb_dim)
+            self.word_embeddings.load_state_dict(temp_state)
+        else:
+            self.emb_dim = len(word_embeddings[0])
+            self.word_len - len(word_embeddings)
 
-                self.word_embeddings =  nn.Embedding.from_pretrained(word_embeddings, freeze=freeze_pretrained_words)
-                temp_state = self.word_embeddings.state_dict()
-                temp_state['weight'][0] = torch.zeros(self.emb_dim)
-                self.word_embeddings.load_state_dict(temp_state)
+            self.word_embeddings =  nn.Embedding.from_pretrained(word_embeddings, freeze=freeze_pretrained_words)
+            temp_state = self.word_embeddings.state_dict()
+            temp_state['weight'][0] = torch.zeros(self.emb_dim)
+            self.word_embeddings.load_state_dict(temp_state)
 
-            self.emoji_embeddings = nn.Embedding(emoji_len, self.emb_dim)
-            temp_state2 = self.emoji_embeddings.state_dict()
-            temp_state2['weight'][0] = torch.zeros(self.emb_dim)
-            self.emoji_embeddings.load_state_dict(temp_state2)
 
-            self.layer_list = [
-                nn.Sequential(
-                    nn.Linear(self.emb_dim, word_len + emoji_len - 1),
-                    nn.Softmax(dim=1)
-                ) for i in range(window)
-            ]
+        # Create Emoji Embeddings, zeroing out 0/the period
+        self.emoji_embeddings = nn.Embedding(emoji_len, self.emb_dim)
+        temp_state2 = self.emoji_embeddings.state_dict()
+        temp_state2['weight'][0] = torch.zeros(self.emb_dim)
+        self.emoji_embeddings.load_state_dict(temp_state2)
 
-        def forward(self, X: torch.Tensor):
-            # Assuming this is data loaded
-            zeroes = torch.zeros_like(X)
-            zeroed_out_emojis = torch.where(X < 0, zeroes, X)
-            zeroed_out_words = torch.where(X < 0, torch.negative(X), zeroes)
-            words = self.word_embeddings(zeroed_out_emojis)
-            emojis = self.emoji_embeddings(zeroed_out_words)
-            embedded = torch.add(emojis, words)
 
-            out = [layer(embedded) for layer in self.layer_list]
-            return torch.stack(out, dim=1)
-        
-        def get_word_embedding(self, word_num):
-            if word_num < 0:
-                return self.emoji_embeddings(-word_num)
-            return self.word_embeddings(word_num)
-        
-        def predict(self, X: torch.tensor):
-            if X.ndim == 1:
-                results = torch.argmax(X)
-            else:
-                results = torch.argmax(X, dim=1)
-            results2 = results - ((self.word_len + self.emoji_len - 1) * torch.ones_like(results))
-            return torch.where(results >= self.word_len, results2, results)
+        # As far as I can tell, Skipgram is word -> embedding -> different layers for different context words
+        self.layer_list = [
+            nn.Sequential(
+                nn.Linear(self.emb_dim, word_len + emoji_len - 1),
+                nn.Softmax(dim=1)
+            ) for i in range(window)
+        ]
+
+    def forward(self, X: torch.Tensor):
+        # This is a mess I came up with on how to word embed all of these across the two since negatives are emoji
+        zeroes = torch.zeros_like(X)
+        zeroed_out_emojis = torch.where(X < 0, zeroes, X)
+        zeroed_out_words = torch.where(X < 0, torch.negative(X), zeroes)
+        words = self.word_embeddings(zeroed_out_emojis)
+        emojis = self.emoji_embeddings(zeroed_out_words)
+        embedded = torch.add(emojis, words)
+
+
+        # Predict all the outpus words, then stack and ship
+        out1 = [layer(embedded) for layer in self.layer_list]
+        out2 = torch.stack(out1, dim=1)
+
+        return out2
+    
+    # Allows retrieval of embeddings
+    def get_word_embeddings(self, words):
+        emojis = self.emoji_embeddings(words)
+        words = self.word_embeddings(words)
+        return torch.where(words < 0, emojis, words)
+    
+    # Converts probabilities into predictions
+    def predict(self, X: torch.tensor):
+        if X.ndim == 1:
+            results = torch.argmax(X)
+        else:
+            results = torch.argmax(X, dim=1)
+        results2 = results - ((self.word_len + self.emoji_len - 1) * torch.ones_like(results))
+        return torch.where(results >= self.word_len, results2, results)
--- test.py
+++ test.py
@@ -21,18 +21,19 @@ parser.add_argument('-E', '--embeddings', choices=['glove', 'word2vec', 'none'],
 parser.add_argument('-e', '--emoji2vec', default=False)
 parser.add_argument('-d', '--hiddenSize', default=20)
 parser.add_argument('-g', '--gpu', default=False)
-parser.add_argument('-l', '--learningrate', default=1)
+parser.add_argument('-l', '--learningrate', default=0.1)
 
 args = parser.parse_args()
 
 window = args.window
 
+# Determine whether to use the gpu
 if args.gpu:
     gpu = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 else:
     gpu = torch.device('cpu')
 
-
+# Determine the Dataset
 if args.model == 'cbow':
     dataset = CBOWDataset(args.filename, window_size=window, device=gpu, emoji_windows_only=True)
 elif args.model == 'skipgram':
@@ -40,6 +41,8 @@ elif args.model == 'skipgram':
     assert(False)
 
 
+
+# Load Embeddings
 word_model = None
 if args.embeddings == 'word2vec':
     word_model = gensim.models.KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin')
@@ -52,6 +55,8 @@ else:
     assert(word_model is None)
     emb_dim = 50
 
+
+# Set up the Correct Loaded Embeddings
 word_weights = None
 if not word_model is None:
     word_list = []
@@ -63,6 +68,8 @@ if not word_model is None:
             word_list.append(torch.zeros(emb_dim))
     word_weights = torch.stack(word_list)
 
+
+# Load Emoji2Vec is neccessary (WIP)
 if args.emoji2vec:
     assert(emb_dim == 300)
     model = gensim.models.KeyedVectors.load_word2vec_format('emoji2vec.bin')
@@ -70,18 +77,15 @@ if args.emoji2vec:
 else:
     emoji_weights = None
 
-
+# Setup Model (Skipgram WIP)
 if args.model == 'cbow':
     model = CBOW(dataset.dict_index, dataset.emoji_index, window=window, emb_dim=emb_dim, word_embeddings=word_weights, hidden_size=args.hiddenSize).to(gpu)
 elif args.model == 'skipgram':
     print('Not Implemented Yet')
     assert(False)
 
-# SWITCH BEFORE TURN IN, SHOULD BE OPPOSITE (testing true, full_sail False)
-full_sail = False
-testing = True
 
-# This was my evaluation function so I could leave it running over night to do parts of it
+# Setup optimizer
 max_iter = 10000
 learn_rate = args.learningrate
 batch_size = 4
@@ -91,23 +95,26 @@ criterion = torch.nn.NLLLoss()
 
 
 data_len = len(dataset)
-#data_len = 4
 batch_count = (data_len -1) // batch_size + 1
 
 print("Starting Training")
 for i in range(1, max_iter + 1):
+    # Train for this iteration in batches
     for j in range (0, batch_count):
-        torch.cuda.empty_cache()
+        if args.gpu and torch.cuda.is_available():
+            torch.cuda.empty_cache()
         batch_start = j * batch_size
         batch_end = min(data_len, (j + 1) * batch_size)
         model.train()
         optimizer.zero_grad()
         pred = model(dataset[batch_start:batch_end].to(gpu))
-        #actual = dataset.getOneHot(slice(batch_start, batch_end, None)).to(gpu)
         actual = dataset.getWordPos(slice(batch_start, batch_end, None))
         loss = criterion(pred, actual)
         loss.backward()
         optimizer.step()
+
+
+    # Evaluate the model
     model.eval()
     criterion.eval()
     correct_list = []
@@ -130,8 +137,5 @@ for i in range(1, max_iter + 1):
     print("Iteration:", i, "Accuracy:", sum(correct_list) / data_len, "Loss:", sum(loss_list) / data_len)
     criterion.train()
     model.train()
-
-
-
 
 print("done")
