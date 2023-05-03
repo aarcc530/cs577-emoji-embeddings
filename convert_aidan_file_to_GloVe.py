ed373fdec6d0397c7f1a7e872f75fad330c96ea7
SMason1441 <54243304+SMason1441@users.noreply.github.com>
2023-05-02 23:02:32 -0400

    Add files via upload

---
+++ convert_aidan_file_to_GloVe.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import emoji
+import argparse
+
+parser = argparse.ArgumentParser(
+                    prog='File converter',
+                    description='Convert CBOW files into proper GloVe/Word2Vec format')
+
+parser.add_argument('-f', '--filename')
+args = parser.parse_args()
+assert args.filename
+
+
+embeddings = pd.read_csv(args.filename)
+embeddings[0] = embeddings.apply(lambda row: emoji.demojize(row[0], language='alias'), axis=1)
+embeddings.drop(embeddings.tail(1).index, inplace=True)
+embeddings['numbers'] = embeddings.apply(lambda row: [float(x[:-1]) for x in row['Embedding'][1:-1].split()], axis=1)
+numbers = pd.DataFrame(embeddings['numbers'].tolist(), columns=range(1, 51))
+numbers.insert(0, 'emoji', embeddings.iloc[:, 0])
+numbers.to_csv(f'{args.filename[:-4]}.txt', sep=' ', header=False, index=False)
